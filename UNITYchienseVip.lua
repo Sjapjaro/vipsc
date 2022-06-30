c3406024d3c217c12cf382a7300eb59cdd6080ed
Sjapjaro <108489339+Sjapjaro@users.noreply.github.com>
2022-06-30 22:23:45 +0300

    Create UNITYchienseVip.lua

---
+++ UNITYchienseVip.lua
@@ -0,0 +1,381 @@
+gg.sleep(300)
+gg.toast(" ◼◻◻◻◻◻◻◻◻◻10%")
+gg.sleep(300)
+gg.toast(" ◼◼◻◻◻◻◻◻◻◻20%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◻◻◻◻◻◻◻30%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◻◻◻◻◻◻40%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◼◻◻◻◻◻50%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◼◼◻◻◻◻60%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◼◼◼◻◻◻70%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◼◼◼◼◻◻80%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◼◼◼◼◼◻90%")
+gg.sleep(300)
+gg.toast(" ◼◼◼◼◼◼◼◼◼◼100%")
+gg.sleep(300)
+gg.alert('ঔৣ͜͡➳ ➣MƛƘЄƊ ƁƳ ЄMƖƦӇƛƝ】@legacy_emirhan/    ঔৣ͜͡➳ ➣Yt=emirhan_unity')
+EMIRHAN = -1 
+function MainMenu()
+WeaponMenu = gg.multiChoice({
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x5】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x15】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x50】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x100】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x150】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x200】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x250】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x300】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x350】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x400】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x450】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x500】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x550】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x600】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x650】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x700】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x750】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x800】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x850】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x900】",
+	"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ x999】",
+	"┏━━❂𝐁𝐀𝐂𝐊❂━━┓",
+	    },nil,"ঔৣ͜͡➳ ➣【ᴄʜɪᴇɴsᴇ sᴘᴇᴇᴅ】")
+
+if WeaponMenu == nil then else
+if WeaponMenu[1] == true then HACK1() end
+if WeaponMenu[2] == true then HACK2() end
+if WeaponMenu[3] == true then HACK3() end
+if WeaponMenu[4] == true then HACK4() end
+if WeaponMenu[5] == true then HACK5() end
+if WeaponMenu[6] == true then HACK6() end
+if WeaponMenu[7] == true then HACK7() end
+if WeaponMenu[8] == true then HACK8() end
+if WeaponMenu[9] == true then HACK9() end
+if WeaponMenu[10] == true then HACK10() end
+if WeaponMenu[11] == true then HACK11() end
+if WeaponMenu[12] == true then HACK12() end
+if WeaponMenu[13] == true then HACK13() end
+if WeaponMenu[14] == true then HACK14() end
+if WeaponMenu[15] == true then HACK15() end
+if WeaponMenu[16] == true then HACK16() end
+if WeaponMenu[17] == true then HACK17() end
+if WeaponMenu[18] == true then HACK18() end
+if WeaponMenu[19] == true then HACK19() end
+if WeaponMenu[20] == true then HACK20() end
+if WeaponMenu[21] == true then HACK21() end
+end
+EMIRHAN = -1 
+end
+
+function HACK1()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "5"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK2()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "15"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK3()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "50"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK4()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "100"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK5()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "150"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK6()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "200"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK7()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "250"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK8()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "300"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK9()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "350"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK10()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "400"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK11()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "450"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK12()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "500"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK13()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "550"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK14()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "600"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK15()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "650"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK16()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "700"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK17()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "750"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK18()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "800"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK19()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "850"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK20()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "900"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+function HACK21()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;0.0700000003;0.02999999933::9", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(1, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(1)
+for i, v in ipairs(t) do
+ t[i].value = "999"
+ t[i].freeze = true
+end
+gg.addListItems(t)
+gg.clearResults()
+end
+
+MainMenu()
+
+while(true)
+do
+  while gg.isVisible(true)
+    do
+      gg.setVisible(false)
+      MainMenu()
+    end
+end
+
